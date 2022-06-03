11c2cfb7c76bbd65dd7c07c5639340b8f0ee2250
arndt-netapp <arndt@netapp.com>
2022-06-03 12:08:55 -0400

    Added aiqum_aggr_report.py example script.

---
+++ aiqum_aggr_report.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+
+################################################################################
+#
+# This sample code shows how to connect to the AIQUM Database and pull aggregate
+# related details from it.
+#
+# AIQUM Requirements:
+#   1. AIQUM 9.7 or higher.
+#   2. An AIQUM "Database User" account with the "Report Schema" role.
+#
+# Python Requirements:
+#   1. The mysql-connector-python module must be installed.
+#
+# AIQUM Database Schema documentation is on the NetApp Support Site:
+# https://mysupport.netapp.com/documentation/docweb/index.html?productID=63834
+#
+################################################################################
+
+import mysql.connector
+import sys
+import datetime
+from argparse import ArgumentParser
+from getpass import getpass
+
+# Connection setup for AIQUM.
+def aiqum_db_connect(aiq_host,aiq_user,aiq_password):
+    try:
+        cnx = mysql.connector.connect(host=aiq_host,
+                                      user=aiq_user,
+                                      password=aiq_password,
+                                      database='netapp_model_view'
+                                     )
+    except:
+        print()
+        print("Error connecting to AIQUM Database. Exiting.")
+        print()
+        raise
+
+    return cnx
+
+# Query AIQUM for aggr data and print in CSV format.
+def aiqum_aggregates(cnx):
+    cursor = cnx.cursor()
+    query = ("SELECT cluster.name,node.name,"
+             "aggr.name,aggr.sizeUsedPercent,aggr.sizeTotal,aggr.sizeUsed,"
+             "cluster.lastUpdateTime "
+             "FROM aggregate AS aggr "
+             "INNER JOIN node ON aggr.nodeId = node.objid "
+             "INNER JOIN cluster ON aggr.clusterId = cluster.objid "
+            )
+    cursor.execute(query)
+
+    print("Cluster,Node,"
+          "Aggregate,PercentUsed,Size(GB),UsedSize(GB),"
+          "LastUpdated")
+    for row in cursor:
+        SizeGB = "%.1f" % (row[4]  / (1024*1024*1024))
+        UsedGB = "%.1f" % (row[5]  / (1024*1024*1024))
+        epochtime = "%i" % (row[6] / 1000)
+        lastupdated = datetime.datetime.fromtimestamp(int(epochtime))
+        print("%s,%s,"
+              "%s,%s,%s,%s,"
+              "%s"
+              %
+              (row[0],row[1],
+               row[2],row[3],SizeGB,UsedGB,
+               lastupdated
+              )
+             )
+
+    return 1
+
+# -----------------------------------------------------------------------------
+# MAIN
+# -----------------------------------------------------------------------------
+
+# Parse the command line
+parser = ArgumentParser(
+    usage="%(prog)s [options]",
+    description="Sample code to pull aggr details from the AIQUM Datbase."
+)
+parser.add_argument(
+    '-a', '--aiqumhost', nargs='?', required=True, help='AIQUM Host'
+)
+parser.add_argument(
+    '-u', '--username', nargs='?', required=True, help='AIQUM Username'
+)
+parser.add_argument(
+    '-p', '--password', nargs='?', help='Password for AIQUM username'
+)
+args = parser.parse_args()
+if not args.password: args.password = getpass()
+
+# Connect to AIQUM and print the aggr details.
+cnx = aiqum_db_connect(args.aiqumhost,args.username,args.password)
+aiqum_aggregates(cnx)
